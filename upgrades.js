6aeffed442052fee8d6a2782d9500fc7974215cb
Wyatt <w.vaughan.2021@gmail.com>
2024-09-30 11:56:34 -0500

    Added upgrade for rejectUnauthorized config

--- upgrades.js
+++ upgrades.js
@@ -3,11 +3,20 @@ export default [
 	 * Place your upgrade scripts here
 	 * Remember that once it has been added it cannot be removed!
 	 */
-	// function (context, props) {
-	// 	return {
-	// 		updatedConfig: null,
-	// 		updatedActions: [],
-	// 		updatedFeedbacks: [],
-	// 	}
-	// },
+	function (context, props) {
+		// rejectUnauthorizedInfo
+		let changed = {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: [],
+		}
+		if (props.config !== null) {
+			let config = props.config
+			if (config.rejectUnauthorized == undefined || config.rejectUnauthorized == '') {
+				config.rejectUnauthorized = true
+				changed.updatedConfig = config
+			}
+		}
+		return changed
+	},
 ]
