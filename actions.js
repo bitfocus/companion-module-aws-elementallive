7793f46b103f153c101294437cffe5e2fe899ee2
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2024-10-30 17:44:16 +1100

    use variables for action id, refactor action options

--- actions.js
+++ actions.js
@@ -1,150 +1,93 @@
+import { actionOptions } from "./options"
+
 export function getActions() {
 	let actions = {
 		startLiveEvent: {
 			name: 'Start Live Event',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
+				actionOptions.id,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let start = { start: '' }
-				this.sendPostRequest(`live_events/${action.options.id}/start`, start)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/start`, start)
 			},
 		},
 		stopLiveEvent: {
 			name: 'Stop Live Event',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
+				actionOptions.id,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let stop = { stop: '' }
-				this.sendPostRequest(`live_events/${action.options.id}/stop`, stop)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/stop`, stop)
 			},
 		},
 		cancelLiveEvent: {
 			name: 'Cancel Live Event',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
+				actionOptions.id,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let cancel = { cancel: '' }
-				this.sendPostRequest(`live_events/${action.options.id}/cancel`, cancel)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/cancel`, cancel)
 			},
 		},
 		archiveLiveEvent: {
 			name: 'Archive Live Event',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
+				actionOptions.id,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let archive = { archive: '' }
-				this.sendPostRequest(`live_events/${action.options.id}/archive`, archive)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/archive`, archive)
 			},
 		},
 		resetLiveEvent: {
 			name: 'Reset Live Event',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
+				actionOptions.id,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let reset = { reset: '' }
-				this.sendPostRequest(`live_events/${action.options.id}/reset`, reset)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/reset`, reset)
 			},
 		},
 		muteAudio: {
 			name: 'Mute/Unmute Audio',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
-				{
-					id: 'mute',
-					type: 'checkbox',
-					label: 'Mute',
-					default: true,
-				},
+				actionOptions.id,
+				actionOptions.mute,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				if (action.options.mute) {
 					let mute = { mute_audio: '' }
-					this.sendPostRequest(`live_events/${action.options.id}/mute_audio`, mute)
+					this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/mute_audio`, mute)
 				} else {
 					let unmute = { unmute_audio: '' }
-					this.sendPostRequest(`live_events/${action.options.id}/unmute_audio`, unmute)
+					this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/unmute_audio`, unmute)
 				}
 			},
 		},
 		setAudioGain: {
 			name: 'Set Audio Gain',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
-				{
-					id: 'gain',
-					type: 'number',
-					label: 'Gain (dB)',
-					default: 0,
-					min: -60,
-					max: 60,
-					range: true,
-				},
+				actionOptions.id,
+				actionOptions.gain,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let audioGain = { gain: action.options.gain }
-				this.sendPostRequest(`live_events/${action.options.id}/adjust_audio_gain`, audioGain)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/adjust_audio_gain`, audioGain)
 			},
 		},
 		insertSCTE35Message: {
 			name: 'Insert SCTE35',
 			options: [
-				{
-					id: 'id',
-					type: 'textinput',
-					label: 'Event ID',
-					default: '',
-				},
-
-				{
-					id: 'duration',
-					type: 'number',
-					label: 'Duration',
-					default: '',
-				},
+				actionOptions.id,
+				actionOptions.duration,
 			],
-			callback: (action) => {
+			callback: async (action) => {
 				let spliceMessage = { cue_point: { duration: action.options.duration, splice_offset: 0 } }
-				this.sendPostRequest(`live_events/${action.options.id}/cue_point`, spliceMessage)
+				this.sendPostRequest(`live_events/${await this.parseVariablesInString(action.options.id)}/cue_point`, spliceMessage)
 			},
 		},
 	}
